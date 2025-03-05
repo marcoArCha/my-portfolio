2a79d32ba7b2023a0e14f839b5ef73bd22c74d44
marcoarcha <marcoarcha@hotmail.com>
2025-03-05 10:18:16 -0600

    Import components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
 import "./App.css";
+import {NavBar} from "./components/NavBar"
+import { MobileMenu } from "./components/MobileMenu";
 
 
 function App() {
 
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   
   return(
-    <><h1>
+    <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-    </h1>
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    </div>
     </>
   )
 }
